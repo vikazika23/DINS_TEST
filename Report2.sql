5cf0efaff2f3d14a014823f562f228ccf2b0cb7f
vikazika23 <izabir23@mail.ru>
2019-08-18 10:16:51 +0300

    Update Report2.sql

--- Report2.sql
+++ Report2.sql
@@ -1,5 +1,5 @@
 select accounts.Name, numbers.Phone_number,zap.c 
-from accounts inner join (select UID, count(*) as c from call_logs group by UID) zap 
+from accounts inner join (select UID, count(*) as c from call_logs group by UID) as zap 
 on zap.UID=accounts.UID inner join numbers 
 on numbers.UID=accounts.UID 
 order by zap.c desc 
